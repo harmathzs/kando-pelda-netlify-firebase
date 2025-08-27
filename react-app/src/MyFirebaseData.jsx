46a3c156ad3e85da9bec86436438ee2f9b3c734a
Zsolt Harmath <harmathzs@gmail.com>
2025-08-27 09:11:59 +0200

    Add more test data for triggering feature branch deployment

--- react-app/src/MyFirebaseData.jsx
+++ react-app/src/MyFirebaseData.jsx
@@ -3,7 +3,7 @@ export default function MyFirebaseData(props) {
     const [firebaseData, setFirebaseData] = useState([]);
 
     useEffect(()=>{
-        setFirebaseData([{test: 'data1'}, {test: 'data2'}]);
+        setFirebaseData([{test: 'data1'}, {test: 'data2'}, {test: 'data3'}]);
     }, [setFirebaseData]);
 
     return <div>
