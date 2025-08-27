8e5acc41fbd9ce58069908f9c24d694c428159ad
Zsolt Harmath <harmathzs@gmail.com>
2025-08-27 09:08:47 +0200

    Add some test data to MyFirebaseData

--- react-app/src/MyFirebaseData.jsx
+++ react-app/src/MyFirebaseData.jsx
@@ -1,6 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 export default function MyFirebaseData(props) {
+    const [firebaseData, setFirebaseData] = useState([]);
+
+    useEffect(()=>{
+        setFirebaseData([{test: 'data1'}, {test: 'data2'}]);
+    }, [setFirebaseData]);
+
     return <div>
         <p>MyFirebaseData</p>
+        <p>{JSON.stringify(firebaseData)}</p>
     </div>
 }
