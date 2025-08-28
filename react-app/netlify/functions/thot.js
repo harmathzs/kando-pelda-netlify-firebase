f2de7753f9933ad4d0ee00de0f82a9e1d11c99d2
Zsolt Harmath <harmathzs@gmail.com>
2025-08-28 10:27:53 +0200

    Fix Thot async

--- react-app/netlify/functions/thot.js
+++ react-app/netlify/functions/thot.js
@@ -1,24 +1,22 @@
 // fetch('/.netlify/functions/thot')
 export const handler = async () => {
-    fetch('https://thot.philo.ulg.ac.be/api/json/thesaurus/scripts')
-    .then(res=>res.json())
-    .then(res => {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                data: res,
-                error: null,
-            }),
-        }
-    })
-    .catch(err => {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                data: null,
-                error: err,
-            }),
-        }
-    });
-
-}
+  try {
+    const res = await fetch('https://thot.philo.ulg.ac.be/api/json/thesaurus/scripts');
+    const data = await res.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data,
+        error: null,
+      }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        data: null,
+        error: err.message || String(err),
+      }),
+    };
+  }
+};
